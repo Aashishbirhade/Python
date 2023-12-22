416dd10643fb248a2df91731f017b6441f75a0e8
Aashishbirhade <134684645+Aashishbirhade@users.noreply.github.com>
2023-12-22 20:37:09 +0530

    Add files via upload

---
+++ string/join_name.py
@@ -0,0 +1,6 @@
+def name(first,last,place):
+    print(f"Hello my name is {first} {last} from {place}")
+f=input('enter your first name :')
+l=input('enter last name :')
+p=input('enter place :')
+print(name(f,l,p))
